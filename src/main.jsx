df9f8caac6293384b259336325607c2b0a722e89
Luiz Bento <32204385+luizhbento@users.noreply.github.com>
2024-04-25 20:56:56 -0300

    Alterando a estrutura do router

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+import Presenca from './pages/Presenca.jsx';
+import Presentes from './pages/Presentes.jsx';
+import Viagem from './pages/Viagem.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/luizevictoria/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/luizevictoria/',
+        element: <Home />,
+      },
+      {
+        path: '/luizevictoria/rsvp',
+        element: <Presenca />,
+      },
+      {
+        path: '/luizevictoria/presentes',
+        element: <Presentes />,
+      },
+      {
+        path: '/luizevictoria/viagem',
+        element: <Viagem />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
