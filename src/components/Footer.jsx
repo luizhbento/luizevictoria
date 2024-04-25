47d3204805c6c241efbf6e1d0e41f842d98c2e65
Luiz Bento <32204385+luizhbento@users.noreply.github.com>
2024-04-24 22:19:27 -0300

    Correções quanto à responsividade na página inicial, footer, entre outros

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,11 +11,13 @@ const Footer = () => {
     <div className="w-[100%] bg-[#7A7E5E] h-16 flex justify-around items-center text-white font-menuFont">
       <div className="flex flex-col items-center">
         <HiSelector size={20} className="inline" />
-        <h2 className=" text-sm md:text-base">Site feito pelo noivo</h2>
+        <h2 className=" text-xs sm:text-sm md:text-base">
+          Site feito pelo noivo
+        </h2>
       </div>
       <button className="flex flex-col items-center" onClick={scrollToTop}>
         <HiUpload size={20} className="inline" />
-        <h2 className=" text-sm md:text-base">Voltar ao Topo</h2>
+        <h2 className=" text-xs sm:text-sm md:text-base">Voltar ao Topo</h2>
       </button>
       <Link
         className="flex flex-col items-center"
@@ -23,7 +25,7 @@ const Footer = () => {
         target="_blank"
       >
         <HiOutlineChat size={20} className="inline" />
-        <h2 className=" text-sm md:text-base">Fale com a gente</h2>
+        <h2 className=" text-xs sm:text-sm md:text-base">Fale com a gente</h2>
       </Link>
     </div>
   );
